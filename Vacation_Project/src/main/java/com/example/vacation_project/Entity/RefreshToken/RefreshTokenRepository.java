6f3642cc3745dc39c0ad779254551487f32a5fa5
최희 <cmlgml094356@gmail.com>
2022-02-06 01:43:35 +0900

    [CREATE] (#1) 로그인/회원가입
    JWT를 이용하여 로그인 회원가입 구현

---
+++ Vacation_Project/src/main/java/com/example/vacation_project/Entity/RefreshToken/RefreshTokenRepository.java
@@ -0,0 +1,9 @@
+package com.example.vacation_project.Entity.RefreshToken;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+
+}
---
+++ Vacation_Project/src/main/java/com/example/vacation_project/Jwt/Details/Details.java
@@ -0,0 +1,50 @@
+package com.example.vacation_project.Jwt.Details;
+
+import com.example.vacation_project.Entity.Account.Account;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@AllArgsConstructor
+public class Details implements UserDetails {
+
+    private final Account account;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return account.getAccountId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}

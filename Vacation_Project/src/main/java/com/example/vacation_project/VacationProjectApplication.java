44a894299ba661f8f3c1537c521945d52e4fa79c
최희 <cmlgml094356@gmail.com>
2022-02-06 01:37:52 +0900

    [CREATE] project
    기본적인 프로젝트 파일 업로드

---
+++ Vacation_Project/src/main/java/com/example/vacation_project/VacationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.vacation_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacationProjectApplication.class, args);
+    }
+
+}
---
+++ Vacation_Project/src/test/java/com/example/vacation_project/VacationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vacation_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

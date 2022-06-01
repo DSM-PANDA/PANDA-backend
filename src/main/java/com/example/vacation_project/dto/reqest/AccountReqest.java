79d53d7694ee0b29fde6121aacd92fef7d9beb53
hee <cmlgml094356@gmail.com>
2022-06-02 07:45:01 +0900

    ✏️ :: password pequest 필드 정규식 수정

--- src/main/java/com/example/vacation_project/dto/reqest/AccountReqest.java
+++ src/main/java/com/example/vacation_project/dto/reqest/AccountReqest.java
@@ -20,8 +20,7 @@ public class AccountReqest {
     private String accountId;
 
     @NotBlank
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{6,20}$",
-            message = "영어, 숫자, 특수문자가 포함되어야 합니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{6,20}$")
     private String password;
 
 }
--- src/main/java/com/example/vacation_project/dto/reqest/LoginRequest.java
+++ src/main/java/com/example/vacation_project/dto/reqest/LoginRequest.java
@@ -3,6 +3,7 @@ package com.example.vacation_project.dto.reqest;
 import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Getter
@@ -13,7 +14,7 @@ public class LoginRequest {
     private String accountId;
 
     @NotBlank
-    @Size(min = 6, max = 20)
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{6,20}$")
     private String password;
 
 }

26e07dfe81d67c54fe00e69d63a456a6865cf432
hee <cmlgml094356@gmail.com>
2022-06-01 16:56:12 +0900

    ✏️ :: PostFacade 사용

--- src/main/java/com/example/vacation_project/service/AccountService.java
+++ src/main/java/com/example/vacation_project/service/AccountService.java
@@ -6,7 +6,7 @@ import com.example.vacation_project.dto.response.PostResponse;
 import com.example.vacation_project.dto.response.PostViewRespones;
 import com.example.vacation_project.entity.account.Account;
 import com.example.vacation_project.entity.post.Post;
-import com.example.vacation_project.entity.post.PostRepository;
+import com.example.vacation_project.facade.PostFacade;
 import com.example.vacation_project.exception.NotFoundException;
 import com.example.vacation_project.service.util.AccountUtil;
 import org.springframework.data.domain.Pageable;
@@ -23,7 +23,7 @@ import java.util.List;
 public class AccountService {
 
     private final AccountUtil accountUtil;
-    private final PostRepository postRepository;
+    private final PostFacade postFacade;
 
     public AccountNameResponse getname() {
 
@@ -38,8 +38,7 @@ public class AccountService {
 
     public PostResponse getpost(Long postId) {
 
-        Post post = postRepository.findById(postId)
-                .orElseThrow(NotFoundException::new);
+        Post post = postFacade.findByPostId(postId);
 
         return PostResponse.builder()
                 .name(post.getName())
@@ -51,7 +50,7 @@ public class AccountService {
 
         Account account = accountUtil.getAccount();
 
-        List<Post> postList = postRepository.findAllByAccountOrderByIdDesc(account,page);
+        List<Post> postList = postFacade.findAllByAccountOrderByIdDesc(account, page);
         List<PostViewRespones> postViewResponseList = new ArrayList<>();
 
         for(Post post : postList) {

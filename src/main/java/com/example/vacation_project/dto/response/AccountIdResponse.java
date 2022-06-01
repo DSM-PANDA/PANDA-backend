df05a9b32f00c87135acf5e3cc284667f99ed597
hee <cmlgml094356@gmail.com>
2022-06-01 16:51:22 +0900

    ✨ :: AccountIdResponse 생성

---
+++ src/main/java/com/example/vacation_project/dto/response/AccountIdResponse.java
@@ -0,0 +1,12 @@
+package com.example.vacation_project.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AccountIdResponse {
+
+    private final String accountId;
+
+}

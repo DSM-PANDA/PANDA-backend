1ff9f59c673659b6a3bf8dab8d31148d82fda7ae
hee <cmlgml094356@gmail.com>
2022-06-02 08:19:27 +0900

    ✏️ :: import문 수정

--- src/main/java/com/example/vacation_project/controller/PostController.java
+++ src/main/java/com/example/vacation_project/controller/PostController.java
@@ -7,14 +7,19 @@ import com.example.vacation_project.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 @RequestMapping("/{accountId}")
 @RequiredArgsConstructor
 @RestController
-
 public class PostController {
 
     private final PostService postService;

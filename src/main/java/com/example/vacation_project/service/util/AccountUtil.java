bb4f84128dd3d391e1a8cf36c660c1a9618bd3f3
hee <cmlgml094356@gmail.com>
2022-06-01 16:53:10 +0900

    ✏️ :: AccountUtil로 합침

--- src/main/java/com/example/vacation_project/service/util/AccountUtil.java
+++ src/main/java/com/example/vacation_project/service/util/AccountUtil.java
@@ -3,7 +3,10 @@ package com.example.vacation_project.service.util;
 import com.example.vacation_project.entity.account.Account;
 import com.example.vacation_project.entity.account.AccountRepository;
 import com.example.vacation_project.exception.NotFoundException;
+import com.example.vacation_project.exception.UnauthorizedException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
@@ -11,10 +14,17 @@ import org.springframework.stereotype.Component;
 public class AccountUtil {
 
     private final AccountRepository accountRepository;
-    private final AuthenticationUtil authenticationUtil;
+
+    public String getAccountId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) {
+            throw new UnauthorizedException();
+        }
+        return authentication.getName();
+    }
 
     public Account getAccount() {
-        return accountRepository.findByAccountId(authenticationUtil.getAccountId())
+        return accountRepository.findByAccountId(getAccountId())
                 .orElseThrow(NotFoundException::new);
     }
 

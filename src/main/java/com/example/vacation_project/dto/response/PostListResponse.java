67373dc4b02838458e235f618b93191562524f9c
hee <cmlgml094356@gmail.com>
2022-06-01 16:48:50 +0900

    ✏️ :: 필드 수정

--- src/main/java/com/example/vacation_project/dto/response/PostListResponse.java
+++ src/main/java/com/example/vacation_project/dto/response/PostListResponse.java
@@ -3,13 +3,12 @@ package com.example.vacation_project.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Builder
 @AllArgsConstructor
 public class PostListResponse {
 
-    List<PostViewRespones> postViewResponseList = new ArrayList<>();
+    private final String accountName;
 
 }
